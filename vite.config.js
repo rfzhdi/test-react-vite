bcf34d414fc4e95796f74a1504102b1f2bfaec7c
Rafi Zuhdi <rafizuhdi09@gmail.com>
2026-04-24 15:16:10 +0700

    commit test

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { qrcode } from 'vite-plugin-qrcode'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), qrcode()],
+  server: {
+    port: 5174
+  },
+  build: {
+    outDir: 'out'
+  }
+})
